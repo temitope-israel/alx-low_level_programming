cfe5ed17ea68fe78f23c6ca622a2b21d9e4349a5
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-02 20:24:29 -0700

    printing with printf() function.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry Point
+ *   ******Print string to the std output
+ *    **************using
+ *     ************************the puts() function
+ *      * Return: Always 0 (Success)
+ *       */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
