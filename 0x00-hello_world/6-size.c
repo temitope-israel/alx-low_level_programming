4251478668d03a2a9f3113df5740db9a8ba023fb
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-02 22:48:55 -0700

    Sizes of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -21,7 +21,7 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(m));
 	printf("Size of a long long int: %llu byte(s)\n",
 	(unsigned long long)sizeof(n));
-	printf("Size of a Float: %.0f byte(s)\n", (float)sizeof(f));
+	printf("Size of a float: %.0f byte(s)\n", (float)sizeof(f));
 
 	return (0);
 }
