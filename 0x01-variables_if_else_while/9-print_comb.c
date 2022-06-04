7ca9a7b4da26cec6dcdec434b962601267d6f35c
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-04 02:45:27 -0700

    print comma separated digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *	main - Entry point
+ *
+ *	Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar((x) + '0');
+		if (x < 9)
+			putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
