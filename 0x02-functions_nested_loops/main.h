b3d0ebfc181d4517ea674318783075dcf5b3ed55
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-07 11:25:56 -0700

    use putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- *
- * Return: Always 0 (Success)
- */
 void print_alphabet(void)
 {
 	char x;
 
 	for (x = 'a'; x <= 'z'; x++)
-		putchar(x);
-	putchar('\n');
+		_putchar(x);
+	_putchar('\n');
 }
