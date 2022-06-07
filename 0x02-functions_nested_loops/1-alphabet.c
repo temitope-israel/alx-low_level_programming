2d6c3a78f4d7d7431aeb7eff4f0a106c6a4c7cda
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-07 13:53:15 -0700

    stdout

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,17 +6,8 @@
  * Return: Always 0.
  */
 
-
-void print_alphabet(void)
-{
-	char x;
-
-	for (x = 'a'; x <= 'z'; x++)
-		putchar(x);
-	putchar('\n');
-}
-
 int main(void)
 {
 	print_alphabet();
-
+	return(0);
+}
