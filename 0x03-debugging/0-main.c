d5b43cb8d16765cf0048610d01af6524dbbfabca
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-09 03:49:56 -0700

    Functiom to check for positive values.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* positive_or_negative - Function to check value
+* passed in is a positive value.
+*
+* @i: value to be checked
+*
+* Return: returns nothing
+*/
+
+void positive_or_negative(int i)
+{
+	if (i >= 0)
+		printf("%d is a positive", i);
+}
+
+
