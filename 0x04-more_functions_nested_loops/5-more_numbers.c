7698251893d5774b41b86970be9309c459c6c0f9
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-10 15:22:54 -0700

    Print 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * more_numbers - Print 10 times the
+ * numbers 0 to 14, followed by a new line.
+ *
+ */
+
+void more_numbers(void)
+{
+	int x, ch;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (ch = 0; ch < 15; ch++)
+		{
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
