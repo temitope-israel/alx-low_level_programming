4a1a08da78e8b457d16aa9b7ce5bddf70d68dab1
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-10 09:54:08 -0700

    Print numbers except 2 & 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * print_numbers - a function that
+ * prints numbers from 0 to 9.
+ *
+ *
+ *
+ * Return: returns nothing.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+	}
+}
