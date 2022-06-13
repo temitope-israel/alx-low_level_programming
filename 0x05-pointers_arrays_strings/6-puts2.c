287303611f396cc70b55e9e19b84aff8d1d05706
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-13 15:30:07 -0700

    Function that prints everyother character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * puts2 - function that prints every other
+ * character of a string, starting with the
+ * first character, followed by a new line.
+ *
+ * @str: parameter
+ *
+ * Return: nothing
+ */
+
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		if (i % 2 == 0)
+			printf("%d", i);
+	}
+	printf("\n");
+}
