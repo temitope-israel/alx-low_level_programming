f8cb6fa01e806415b44b86b74400b9253e3d7322
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-15 04:12:27 -0700

    Added main.h file

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define  _MAIN_H
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+
+#endif
