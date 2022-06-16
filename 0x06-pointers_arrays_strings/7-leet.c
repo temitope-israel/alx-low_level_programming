a8fc34b1c3a4215dd83e3df33a98ca5209949a81
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-16 11:18:53 -0700

    function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,24 +15,18 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+
+	char *x = "aAeEoOtTlL";
+	char *y = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'A' || s[i] == 'a')
-			s[i] = '4';
-
-		if (s[i] == 'E' || s[i] == 'e')
-			s[i] = '3';
-
-		if (s[i] == 'O' || s[i] == 'o')
-			s[i] = '0';
-
-		if (s[i] == 'T' || s[i] == 't')
-			s[i] = '7';
-
-		if (s[i] == 'L' || s[i] == 'l')
-			s[i] = '1';
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == x[j])
+				s[i] = y[j];
+		}
 	}
 
 	return (s);
