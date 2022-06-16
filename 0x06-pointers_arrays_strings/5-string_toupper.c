1baf4f1578a78489c241db31a279e7684d38a502
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-15 21:59:44 -0700

    function that convert strings to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_toupper - function that
+ * changes all lowercase letters
+ * of a string to uppercase.
+ *
+ * @str: string to be converted
+ *
+ * Return: Returns str
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'a') && (str[i] <= 'z'))
+			str[i] = str[i] - 32;
+	}
+
+	return (str);
+}
