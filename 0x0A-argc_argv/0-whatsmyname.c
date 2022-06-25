8b8a400a13bf7fca4cfbeb63fad543e3b8e8e3a0
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-24 20:35:20 -0700

    Program that print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program entry point.
+ *
+ * @argc: param1
+ *
+ * @argv: param2
+ *
+ * Return: return 0.
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
