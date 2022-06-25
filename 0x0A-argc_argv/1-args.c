f3ba7bf23ea236883ca7baf466fabd293c2d8e31
temitope-israel <omoniyitemitopeisrael@gmail.com>
2022-06-24 20:53:55 -0700

    Program that prints number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program entry point.
+ *
+ * @argc: param1
+ *
+ * @argv: param2
+ *
+ * Return: return 0.
+ *
+ */
+
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%i\n", argc);
+	return (0);
+}
